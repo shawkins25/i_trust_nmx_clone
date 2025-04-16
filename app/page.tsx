584ba6089caa73c246e927f93871b15e2530e127
Sam Hawkins <shawkins@nutramaxlabs.com>
2025-04-16 16:11:47 -0400

    small change

--- app/page.tsx
+++ app/page.tsx
@@ -190,13 +190,13 @@ export default function Home() {
             <h2>Clinic Staff</h2>
             <h3>program</h3>
             <h4>Sign up now and enjoy 15% off items for your personal pets.</h4>
-            <Link
+            <div
               className={classes.sign_up_button}
-              href="https://mynutramax.com/register/?utm_source=itrustnutramax_lp&utm_medium=Website&utm_campaign=itrust_banner"
-              target="_blank"
+              // href="https://mynutramax.com/register/?utm_source=itrustnutramax_lp&utm_medium=Website&utm_campaign=itrust_banner"
+              // target="_blank"
             >
               Sign Up
-            </Link>
+            </div>
           </div>
         </Link>
         <section id="testimonials" className={classes.section_4}>
