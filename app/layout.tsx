2d14e5b9a57f60ed82a4b842e20a5ebb539714fc
Sam Hawkins <shawkins@nutramaxlabs.com>
2025-04-17 14:49:08 -0400

    favicon test

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any"/>
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
