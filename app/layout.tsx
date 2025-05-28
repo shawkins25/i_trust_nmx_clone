a35fb857ef623c9350417cff5ce9adc1ef685d00
Sam Hawkins <shawkins@nutramaxlabs.com>
2025-05-28 11:46:10 -0400

    adding LinkedIn and Meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Roboto } from "next/font/google";
 import Analytics from "./analytics";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 const roboto = Roboto({
   weight: ["300", "400", "600", "700", "900"],
@@ -26,10 +27,54 @@ export default function RootLayout({
     <html lang="en" className={roboto.className}>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta name="google-site-verification" content="my-verification-code" />
       </head>
       <body>
-        <Analytics />
-        <SpeedInsights />
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`
+     _linkedin_partner_id = "6951162";
+     window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+     window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+   `}
+        </Script>
+        <Script
+          id="linkedin-loader"
+          strategy="afterInteractive"
+          src="https://snap.licdn.com/li.lms-analytics/insight.min.js"
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=6951162&fmt=gif"
+          />
+        </noscript>
+        {/* Facebook/Meta Pixel */}
+        <Script id="facebook-pixel-init" strategy="afterInteractive">
+          {`
+     !function(f,b,e,v,n,t,s)
+     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+     n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+     if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+     n.queue=[];t=b.createElement(e);t.async=!0;
+     t.src=v;s=b.getElementsByTagName(e)[0];
+     s.parentNode.insertBefore(t,s)}(window, document,'script',
+     'https://connect.facebook.net/en_US/fbevents.js');
+     fbq('init', '689892560456638');
+     fbq('track', 'PageView');
+   `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=689892560456638&ev=PageView&noscript=1"
+          />
+        </noscript>
         {children}
       </body>
     </html>
