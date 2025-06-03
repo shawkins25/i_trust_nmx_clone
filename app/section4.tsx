701dff8bbf6f2c82bddf898fb8ca4edb54c1a99f
Sam Hawkins <shawkins@nutramaxlabs.com>
2025-06-03 14:06:00 -0400

    round 2, FIGHT!

--- app/section4.tsx
+++ app/section4.tsx
@@ -41,10 +41,11 @@ const Section4 = () => {
       <div className={classes.section_4_content_container}>
         <h3>Recommended by Veterinary Professionals</h3>
         <h4>
-          Veterinarians don't just trust Nutramax—they confidently recommend it.
-          From enhancing patient care to supporting quality of life, Nutramax is
-          the #1 Veterinarian Recommended Supplement Company<sup>▼</sup>. See
-          why these vets proudly say, "I Trust Nutramax."
+          Veterinarians don&apos;t just trust Nutramax—they confidently
+          recommend it. From enhancing patient care to supporting quality of
+          life, Nutramax is the #1 Veterinarian Recommended Supplement Company
+          <sup>▼</sup>. See why these vets proudly say, &quot;I Trust
+          Nutramax.&quot;
         </h4>
       </div>
       {/* Only one testimonial on mobile */}
