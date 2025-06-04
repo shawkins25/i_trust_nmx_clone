9964d8b803cc97416747ed250dbd4fa4e8c86039
Sam Hawkins <shawkins@nutramaxlabs.com>
2025-06-04 10:13:54 -0400

    linked in tag adjustment

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,41 +32,45 @@ export default function RootLayout({
         <Analytics />
         <SpeedInsights />
         {/* LinkedIn Insight Tag */}
-        <Script id="linkedin-insight" strategy="afterInteractive">
-          {`
-     _linkedin_partner_id = "6951162";
-     window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-     window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-   `}
+        <Script id="linkedin-init" strategy="afterInteractive">
+          {`_linkedin_partner_id = "7274042";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);`}
         </Script>
-        <Script
-          id="linkedin-loader"
-          strategy="afterInteractive"
-          src="https://snap.licdn.com/li.lms-analytics/insight.min.js"
-        />
+        <Script id="linkedin-loader" strategy="afterInteractive">
+          {`(function(l) {
+            if (!l) {
+              window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q = [];
+            }
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript"; b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);
+          })(window.lintrk);`}
+        </Script>
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: "none" }}
             alt=""
-            src="https://px.ads.linkedin.com/collect/?pid=6951162&fmt=gif"
+            src="https://px.ads.linkedin.com/collect/?pid=7274042&fmt=gif"
           />
         </noscript>
         {/* Facebook/Meta Pixel */}
         <Script id="facebook-pixel-init" strategy="afterInteractive">
-          {`
-     !function(f,b,e,v,n,t,s)
-     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-     n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-     if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-     n.queue=[];t=b.createElement(e);t.async=!0;
-     t.src=v;s=b.getElementsByTagName(e)[0];
-     s.parentNode.insertBefore(t,s)}(window, document,'script',
-     'https://connect.facebook.net/en_US/fbevents.js');
-     fbq('init', '689892560456638');
-     fbq('track', 'PageView');
-   `}
+          {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '689892560456638');
+            fbq('track', 'PageView');`}
         </Script>
         <noscript>
           <img
@@ -83,7 +87,7 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
         <Script id="ttd-init-wrapper" strategy="afterInteractive">
- {`
+          {`
    // Wait until the loader script is available
    function waitForTTDPixel(retries = 10) {
      if (typeof ttd_dom_ready === "function" && typeof TTDUniversalPixelApi === "function") {
@@ -99,7 +103,7 @@ export default function RootLayout({
    }
    waitForTTDPixel();
  `}
-</Script>
+        </Script>
         {children}
       </body>
     </html>
