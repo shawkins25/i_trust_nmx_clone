8d9dd40d96a22346421d3c3029a8105e26a26715
Sam Hawkins <shawkins@nutramaxlabs.com>
2025-09-17 11:38:09 -0400

    hmm

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -5,25 +5,21 @@ import Script from "next/script";
 const GA_MEASUREMENT_ID = "G-3E7L4Q3NJN";
 
 export default function Analytics() {
-
-    return (
-        <>
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-                strategy="afterInteractive"
-            />
-            <Script id="gtag-init" strategy="afterInteractive">
-
-                {`
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${GA_MEASUREMENT_ID}');
 
         `}
-            </Script>
-        </>
-
-    );
-
+      </Script>
+    </>
+  );
 }
